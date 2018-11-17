7ef55ba909e6a5c925a8f2f451774b6c40a97c3b
sarah-veness <sarah.veness@shopify.com>
2018-11-17 12:34:24 -0800

    cards

--- src/components/partials/genericCard.vue
+++ src/components/partials/genericCard.vue
@@ -1,78 +1,36 @@
 <template>
-  <div class='flex'>
-    <md-card md-with-hover>
-      <md-ripple>
-        <md-card-header>
-          <div class="md-title">Card with hover effect</div>
-          <div class="md-subhead">It also have a ripple</div>
-        </md-card-header>
+  <md-card md-with-hover>
+    <md-ripple>
+      <md-card-header>
+        <div class="md-title">{{department.name}}</div>
+      </md-card-header>
 
-        <md-card-content>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
-        </md-card-content>
+      <md-card-content>
+        This is a description about the department.
+      </md-card-content>
 
-      </md-ripple>
-    </md-card>
-    <md-card md-with-hover>
-      <md-ripple>
-        <md-card-header>
-          <div class="md-title">Card with hover effect</div>
-          <div class="md-subhead">It also have a ripple</div>
-        </md-card-header>
-
-        <md-card-content>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
-        </md-card-content>
-
-      </md-ripple>
-    </md-card>
-    <md-card md-with-hover>
-      <md-ripple>
-        <md-card-header>
-          <div class="md-title">Card with hover effect</div>
-          <div class="md-subhead">It also have a ripple</div>
-        </md-card-header>
-
-        <md-card-content>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
-        </md-card-content>
-
-      </md-ripple>
-    </md-card>
-  </div>
+    </md-ripple>
+  </md-card>
 </template>
 
 <script>
 export default {
-  name: 'RegularCards'
+  name: 'RegularCards',
+  props: {
+    department: Object,
+    divisions: Object
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-.flex {
-  display: flex;
-  flex-wrap: wrap;
-}
-
-.md-card {
-  width: 45%;
-  margin: 1% auto !important;
-  padding: 2%;
-}
 
 .md-card-header, .md-card-content {
   text-align: center;
 }
 
-@media only screen and (max-width: 600px) {
-  .flex {
-    flex-direction: column;
-  }
-
-  .md-card {
-    width: 100%;
-  }
+.md-card-content {
+  text-align: center !important;
 }
-
 
 </style>
